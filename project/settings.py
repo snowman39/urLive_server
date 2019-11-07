1bd02df1203906295a13a6cdd44264a21780f478
이재은 <jaeeun@ijaeeun-ui-MacBookPro.local>
2019-11-08 02:57:58 +0900

    dhidsk

--- project/settings.py
+++ project/settings.py
@@ -71,26 +71,17 @@ TEMPLATES = [
     },
 ]
 
-    
-
 WSGI_APPLICATION = 'project.wsgi.application'
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-
-
 DATABASES = {
     'default': {
         'ENGINE' : 'django.db.backends.sqlite3',
         'NAME' : os.path.join(BASE_DIR, 'db.sqlite3',)
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
-
-
 
 AUTH_PASSWORD_VALIDATORS =[
     {
@@ -106,11 +97,8 @@ AUTH_PASSWORD_VALIDATORS =[
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
-
-
 
 
 LANGUAGE_CODE = 'ko-kr'
@@ -127,7 +115,6 @@ USE_TZ= True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS =(
@@ -135,4 +122,3 @@ STATICFILES_DIRS =(
 )
 db_from_env = dj_database_url.config(conn_max_age=git 500)
 DATABASES['default'].update(db_from_env)
-
