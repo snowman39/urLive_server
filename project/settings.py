69aded32da0873601e590f5dd94566850cc6627d
이재은 <jaeeun@ijaeeun-ui-MacBookPro.local>
2019-11-08 02:09:04 +0900

    ㅇㄴ

--- project/settings.py
+++ project/settings.py
@@ -120,10 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = (
-# 	os.path.join(BASE_DIR, 'urlive', 'static'),
-# )
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+	os.path.join(BASE_DIR, 'urlive', 'static'),
+)
 
-db_from_env = dj_database_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config(conn_max_age=git 500)
 DATABASES['default'].update(db_from_env)
