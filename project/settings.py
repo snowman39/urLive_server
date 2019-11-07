9c350c5d23165e53ab1829bdd1872066a3e21a39
이재은 <jaeeun@ijaeeun-ui-MacBookPro.local>
2019-11-08 02:55:36 +0900

    dsdd33ss2222

--- project/settings.py
+++ project/settings.py
@@ -65,12 +65,12 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
             ],
         },
-        
-    },
-
+    },
 ]
+
     
 
 WSGI_APPLICATION = 'project.wsgi.application'
